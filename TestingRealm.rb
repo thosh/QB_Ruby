cf52c349761687ad29ae0e64081934492c5a9bf2
Tom Shannon <tomshannon@email.com>
2013-10-07 01:11:32 -0600

    Update TestingRealm.rb
    Removed credentials

--- TestingRealm.rb
+++ TestingRealm.rb
@@ -1,4 +1,4 @@
 require 'quickbase_client'
 
-qbc = QuickBase::Client.init({"username" => "tom.shannon@regencylighting.com", "password" => "R3g3ncy.c0m",  "org" => "regencylighting"}) 
+qbc = QuickBase::Client.init({"username" => "", "password" => "",  "org" => "regencylighting"}) 
 qbc.grantedDBs{|db|    puts "dbid: #{db.dbinfo.dbid}, dbname: #{db.dbinfo.dbname}" } 
